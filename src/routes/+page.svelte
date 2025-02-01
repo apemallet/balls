4f9d6b7e8d5d781c4adb4dc35ee08fda2f383782
Simon Walker <simon@simonwalker.tech>
2025-02-01 01:10:56 -0700

    basic matterjs setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,42 @@
-<script>
-  import { onMount } from "svelte";
+<script lang="ts">
+    import {onMount} from "svelte";
 
-  onMount(async () => {
-    const Matter = await import("matter-js");
-    const { Engine, Render, Runner, Bodies, Composite } = Matter;
+    let canvas: HTMLCanvasElement;
 
-    // create an engine
-    const engine = Engine.create();
+    onMount(async () => {
+        const Matter = await import("matter-js");
+        const {Engine, Render, Runner, Bodies, Composite} = Matter;
 
-    // create a renderer
-    const render = Render.create({
-      element: document.body,
-      engine: engine,
+        // create an engine
+        const engine = Engine.create();
+
+        const width = canvas.getBoundingClientRect().width
+        const height = canvas.getBoundingClientRect().height
+
+        const render = Render.create({
+            engine: engine,
+            canvas: canvas,
+            options: {
+                width: width,
+                height: height,
+            }
+        });
+
+        // create two boxes and a ground
+        const boxA = Bodies.rectangle(400, 300, 80, 80);
+        const boxB = Bodies.rectangle(450, 50, 80, 80);
+        const ground = Bodies.rectangle(.5 * width, 610, width/4, 60, {isStatic: true});
+
+        // add all the bodies to the world
+        Composite.add(engine.world, [boxA, boxB, ground]);
+
+        // run the renderer
+        Render.run(render);
+
+        // create runner
+        const runner = Runner.create();
+        Runner.run(runner, engine);
     });
-
-    // create two boxes and a ground
-    const boxA = Bodies.rectangle(400, 200, 80, 80);
-    const boxB = Bodies.rectangle(450, 50, 80, 80);
-    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    // add all the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
-
-    // run the renderer
-    Render.run(render);
-
-    // create runner
-    const runner = Runner.create();
-    Runner.run(runner, engine);
-  });
 </script>
+
+<canvas class="w-screen h-screen" bind:this={canvas}></canvas>
