ad129543f46e995bc31885673c0b74207a1be4cb
masoniis <bott.m@comcast.net>
2025-02-01 00:25:31 -0700

    flake improvements, fix

--- flake.nix
+++ flake.nix
@@ -4,16 +4,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-          ];
-
           packages = with pkgs; [
             bun
           ];
@@ -21,9 +21,3 @@
       }
     );
 }
-
-
-
-
-
-
