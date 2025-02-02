ecc1485df1869d427e8db7d5067150a9dbc1c2d6
Simon Walker <simon@simonwalker.tech>
2025-02-01 22:29:55 -0700

    attempting sveltized matterjs integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,52 @@
 <script lang="ts">
 	import ThemePicker from "$lib/components/ThemePicker.svelte";
-	import { onMount } from "svelte";
+	import { createThemer } from "$lib/theme.svelte";
+	import Matter from "$lib/svelteMatter.svelte";
 
-	let canvas: HTMLCanvasElement;
+	let canvas: HTMLCanvasElement | undefined;
+	let width = $derived(canvas?.getBoundingClientRect().width);
+	let height = $derived(canvas?.getBoundingClientRect().height);
 
-	onMount(async () => {
-		const Matter = await import("matter-js");
-		const { Engine, Render, Runner, Bodies, Composite } = Matter;
+	let colorThemer = createThemer();
 
-		// create an engine
-		const engine = Engine.create();
+	const { Engine, Render, Runner, Bodies, Composite } = Matter();
 
-		const width = canvas.getBoundingClientRect().width;
-		const height = canvas.getBoundingClientRect().height;
+	// create an engine
+	const engine = Engine.create();
 
-		// TODO: need a better way to update colors of matter objects so that it works with themePicker changes
-		const rootStyle = getComputedStyle(document.documentElement);
-		const primaryAccent = rootStyle.getPropertyValue("--color-accentbg").trim();
-
-		const render = Render.create({
-			engine: engine,
-			canvas: canvas,
-			options: {
-				width: width,
-				height: height,
-				wireframes: false,
-				background: "transparent",
-			},
-		});
-
-		// create two boxes and a ground
-		const boxA = Bodies.rectangle(400, 300, 80, 80, {
-			render: {
-				fillStyle: primaryAccent,
-			},
-		});
-		const boxB = Bodies.rectangle(450, 50, 80, 80);
-		const ground = Bodies.rectangle(0.5 * width, 610, width / 4, 60, {
-			isStatic: true,
-		});
-
-		// add all the bodies to the world
-		Composite.add(engine.world, [boxA, boxB, ground]);
-
-		// run the renderer
-		Render.run(render);
-
-		// create runner
-		const runner = Runner.create();
-		Runner.run(runner, engine);
+	const render = Render.create({
+		engine: engine,
+		canvas: canvas,
+		options: {
+			width: width,
+			height: height,
+			wireframes: false,
+			background: "transparent",
+		},
 	});
+
+	// create two boxes and a ground
+	const boxA = Bodies.rectangle(400, 300, 80, 80, {
+		render: {
+			fillStyle: colorThemer.color,
+		},
+	});
+
+	const boxB = Bodies.rectangle(450, 50, 80, 80);
+
+	const ground = Bodies.rectangle(0.5 * width, 610, width / 4, 60, {
+		isStatic: true,
+	});
+
+	// add all the bodies to the world
+	Composite.add(engine.world, [boxA, boxB, ground]);
+
+	// run the renderer
+	Render.run(render);
+
+	// create runner
+	const runner = Runner.create();
+	Runner.run(runner, engine);
 </script>
 
 <div class="fixed">
