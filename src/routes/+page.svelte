16932df4e66f6b702ae8a6560c5b3bd57a001ec0
masoniis <bott.m@comcast.net>
2025-02-03 11:15:59 -0700

    main page theme bar fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-  import ThemePicker from "$lib/components/ThemePicker.svelte";
-  import Matter from "$lib/svelteMatter.svelte";
-  import {BallsSim} from "$lib/ballsSim.svelte";
-  import {CrankSim} from "$lib/crankSim.svelte";
-  import {getThemer} from "$lib/theme.svelte";
+	import ThemePicker from "$lib/components/ThemePicker.svelte";
+	import Matter from "$lib/svelteMatter.svelte";
+	import { BallsSim } from "$lib/ballsSim.svelte";
+	import { CrankSim } from "$lib/crankSim.svelte";
+	import { getThemer } from "$lib/theme.svelte";
 
-  let canvasBot: HTMLCanvasElement | undefined;
-  let canvasTop: HTMLCanvasElement | undefined;
-  let balls: BallsSim;
-  let crank: CrankSim;
+	let canvasBot: HTMLCanvasElement | undefined;
+	let canvasTop: HTMLCanvasElement | undefined;
+	let balls: BallsSim;
+	let crank: CrankSim;
 
-  const wheelRadius = 300; // in planck units
+	const wheelRadius = 300; // in planck units
 
-  $effect(() => {
-    if (!Matter()) return;
-    if (!canvasBot) return;
-    if (!canvasTop) return;
-    if (!balls) balls = new BallsSim(canvasBot, wheelRadius);
-    if (!crank) crank = new CrankSim(canvasTop, wheelRadius);
-  });
+	$effect(() => {
+		if (!Matter()) return;
+		if (!canvasBot) return;
+		if (!canvasTop) return;
+		if (!balls) balls = new BallsSim(canvasBot, wheelRadius);
+		if (!crank) crank = new CrankSim(canvasTop, wheelRadius);
+	});
 
-  $effect(() => {
-    if (getThemer().dominant) {
-      if (!balls) return;
-      if (!crank) return;
-      balls!.reTheme();
-      crank!.reTheme();
-    }
-  });
+	$effect(() => {
+		if (getThemer().dominant) {
+			if (!balls) return;
+			if (!crank) return;
+			balls!.reTheme();
+			crank!.reTheme();
+		}
+	});
 
-  function onResize() {
-    const {innerWidth, innerHeight} = window;
-    balls?.reLayout(innerWidth, innerHeight);
-    crank?.reLayout(innerWidth, innerHeight);
-  }
+	function onResize() {
+		const { innerWidth, innerHeight } = window;
+		balls?.reLayout(innerWidth, innerHeight);
+		crank?.reLayout(innerWidth, innerHeight);
+	}
 </script>
 
-<svelte:window on:resize={onResize}/>
+<svelte:window on:resize={onResize} />
 
-<div class="fixed z-20">
-  <ThemePicker/>
+<div class="fixed z-20 w-screen p-4">
+	<ThemePicker />
 </div>
 
 <canvas class="absolute w-screen h-screen z-10" bind:this={canvasTop}></canvas>
