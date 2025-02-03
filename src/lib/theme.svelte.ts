d570c3eb8d1e48ab47cc7588b1e72852c8fd1531
masoniis <bott.m@comcast.net>
2025-02-02 22:20:04 -0700

    analogous color working, chroma js added to bun

--- src/lib/theme.svelte.ts
+++ src/lib/theme.svelte.ts
@@ -1,4 +1,9 @@
 import { browser } from "$app/environment";
+import chroma from "chroma-js";
+
+console.log(chroma('#D4F880').darken().hex());
+console.log(chroma.hsl(32, 1, 0.5).hex()); // S=100%, L=50%
+console.log(chroma(32, 1, 0.5, 'hsl').hex())
 
 // Constants
 const DEFAULT_DOMINANT = "#e7bad0";
@@ -60,6 +65,11 @@ export function createThemer(): Themer {
 				updateTheme(dominant);
 		});
 
+		$effect(() => {
+			console.log(`Dom color changed: ${dominant}`)
+			console.log(`alt1: ${computeAnalogous(dominant)[0]}`)
+		})
+
     themerInstance = {
 				get colorHarmony(): ColorHarmony { return colorHarmony },
 				set colorHarmony(value: ColorHarmony) { colorHarmony = value },
@@ -107,105 +117,23 @@ function getContrastingColor(accentColor: string): string {
 }
 
 function calculateContrast(hexVal1: string, hexVal2: string): number {
-	const color1 = hexToRgb(hexVal1)!;
-	const color2 = hexToRgb(hexVal2)!;
+	const color1 = chroma(hexVal1).rgb();
+	const color2 = chroma(hexVal2).rgb();
   const l1 = 0.2126 * color1.r + 0.7152 * color1.g + 0.0722 * color1.b;
   const l2 = 0.2126 * color2.r + 0.7152 * color2.g + 0.0722 * color2.b;
   return Math.abs(l1 - l2) / 255;
 }
 
-function rgbToHex(r: number, g: number, b: number): string {
-	function componentToHex(c: number) {
-		var hex = c.toString(16);
-		return hex.length == 1 ? "0" + hex : hex;
-	}
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function hexToRgb(hex: string): { r: number; g: number; b: number } {
-  const color = hex
-    .replace("#", "")
-    .match(/[0-9a-fA-F]{2}/g)!
-    .map((x) => parseInt(x, 16));
-  return {
-    r: color[0],
-    g: color[1],
-    b: color[2]
-  };
-}
-
-function hslToHex(hsl: { h: number; s: number; l: number }): string {
-  const { h, s, l } = hsl;
-
-  const hDecimal = l / 100;
-  const a = (s * Math.min(hDecimal, 1 - hDecimal)) / 100;
-  const f = (n: number) => {
-    const k = (n + h / 30) % 12;
-    const color = hDecimal - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-
-    // Convert to Hex and prefix with "0" if required
-    return Math.round(255 * color)
-      .toString(16)
-      .padStart(2, "0");
-  };
-  return `#${f(0)}${f(8)}${f(4)}`;
-}
-
-function hexToHsl(hex: string): { h: number; s: number; l: number } {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-  if (!result) {
-    throw new Error("Could not parse Hex Color");
-  }
-
-  const rHex = parseInt(result[1], 16);
-  const gHex = parseInt(result[2], 16);
-  const bHex = parseInt(result[3], 16);
-
-  const r = rHex / 255;
-  const g = gHex / 255;
-  const b = bHex / 255;
-
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
-
-  let h = (max + min) / 2;
-  let s = h;
-  let l = h;
-
-  if (max === min) {
-    // Achromatic
-    return { h: 0, s: 0, l };
-  }
-
-  const d = max - min;
-  s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-  switch (max) {
-    case r:
-      h = (g - b) / d + (g < b ? 6 : 0);
-      break;
-    case g:
-      h = (b - r) / d + 2;
-      break;
-    case b:
-      h = (r - g) / d + 4;
-      break;
-  }
-  h /= 6;
-
-  s = s * 100;
-  s = Math.round(s);
-  l = l * 100;
-  l = Math.round(l);
-  h = Math.round(360 * h);
-
-  return { h, s, l };
-}
 
 // Color harmony functions
 function computeAnalogous(dominantHex: string) {
-	// TODO 
-	return [dominantHex, dominantHex, dominantHex, dominantHex, dominantHex];
+	const dHSL = chroma(dominantHex).hsl();
+
+	const steps = [-30, -15, 0, 15, 30]
+  return steps.map(step => {
+    const hue = (dHSL[0] + step + 360) % 360;
+		return chroma(hue, 1, 0.5, 'hsl').hex(); // S=100%, L=50%
+  });
 }
 
 function computeMonochromatic(dominantHex: string) {
