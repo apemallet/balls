bc596a7c06c233564c4d77b07c80c1d4cdaffc56
masoniis <bott.m@comcast.net>
2025-02-04 15:44:22 -0700

    adding a winner history modal

---
+++ src/lib/components/WinHistModal.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Modal from "./Modal.svelte";
+	let { showModal = $bindable(false) } = $props();
+</script>
+
+<Modal bind:isOpen={showModal}>
+	<svelte:fragment slot="title">Winner history</svelte:fragment>
+
+	<svelte:fragment slot="content">
+		{" "}
+	</svelte:fragment>
+</Modal>
