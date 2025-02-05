8f65748481a3bb25095aafa4a5d41d1ede5226cd
masoniis <bott.m@comcast.net>
2025-02-05 12:49:20 -0700

    removed a single period character

--- src/lib/components/InfoModal.svelte
+++ src/lib/components/InfoModal.svelte
@@ -4,18 +4,18 @@
 </script>
 
 <Modal bind:isOpen={showModal}>
-	<svelte:fragment slot="title">Details, shmetails, code</svelte:fragment>
+	<svelte:fragment slot="title">Details, shmetails, and code</svelte:fragment>
 
 	<svelte:fragment slot="content">
-		Made in full by <b>simon walker</b> and <b>mason bott</b>.
+		Made in full by <b>simon walker</b> and <b>mason bott</b>
 		<li>Open to any criticisms (no promises on acting on them though 😔)</li>
-		<li>Made with SvelteKit, TailwindCSS, and TypeScript. 🚀</li>
+		<li>Made with SvelteKit, TailwindCSS, and TypeScript 🚀</li>
 		<li>
 			<a
 				href="https://github.com/apemallet/balls"
 				class="text-dominantbg/60 hover:text-dominantbg/80"
 			>
-				Click here to see the code! 🔥🔥🔥
+				Click here to see the code 🔥🔥🔥
 			</a>
 		</li>
 	</svelte:fragment>
