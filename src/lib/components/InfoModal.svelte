7471ffd4f4341e203ebdebc3d0c55f8cf032ae62
masoniis <bott.m@comcast.net>
2025-02-08 15:12:48 -0700

    hyperlink sites, grabber to pointer

--- src/lib/components/InfoModal.svelte
+++ src/lib/components/InfoModal.svelte
@@ -7,16 +7,25 @@
 	<svelte:fragment slot="title">Details, shmetails, and code</svelte:fragment>
 
 	<svelte:fragment slot="content">
-		Made in full by <b>simon walker</b> and <b>mason bott</b>
+		Made in full by <a href="https://masonbott.com" target="_blank"
+			>Mason Bott</a
+		>
+		and
+		<a href="https://simonwalker.tech" target="_blank">Simon Walker</a>
 		<li>Open to any criticisms (no promises on acting on them though 😔)</li>
 		<li>Made with SvelteKit, TailwindCSS, and TypeScript 🚀</li>
 		<li>
-			<a
-				href="https://github.com/apemallet/balls"
-				class="text-dominantbg/60 hover:text-dominantbg/80"
-			>
+			<a href="https://github.com/apemallet/balls" target=" _blank">
 				Click here to see the code 🔥🔥🔥
 			</a>
 		</li>
 	</svelte:fragment>
 </Modal>
+
+<style>
+	@reference "tailwindcss/theme";
+	@reference "../../app.css";
+	a {
+		@apply text-dominantbg/60 hover:text-dominantbg/80 underline underline-offset-2 hover:underline-offset-4 transition-all;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,150 +6,150 @@
 	import WinModal from "$lib/components/WinModal.svelte";
 	import WinHistModal from "$lib/components/WinHistModal.svelte";
 	import Matter from "$lib/svelteMatter.svelte";
-	import {type Winner} from "$lib/winTypes";
-	import {BallsSim} from "$lib/ballsSim.svelte";
-	import {CrankSim} from "$lib/crankSim.svelte";
-	import {getThemer} from "$lib/theme.svelte";
-	import {onMount} from "svelte";
+	import { type Winner } from "$lib/winTypes";
+	import { BallsSim } from "$lib/ballsSim.svelte";
+	import { CrankSim } from "$lib/crankSim.svelte";
+	import { getThemer } from "$lib/theme.svelte";
+	import { onMount } from "svelte";
 
 	let canvasBot: HTMLCanvasElement | undefined;
-  let canvasTop: HTMLCanvasElement | undefined;
-  let balls: BallsSim;
-  let crank: CrankSim;
-  let canCrank = $state(true); // prevents rapid cranking
+	let canvasTop: HTMLCanvasElement | undefined;
+	let balls: BallsSim;
+	let crank: CrankSim;
+	let canCrank = $state(true); // prevents rapid cranking
 
-  let lastWinner: Winner | undefined = $state(); // initial value should be unreachable
-  const wheelRadius = 300; // in planck units
+	let lastWinner: Winner | undefined = $state(); // initial value should be unreachable
+	const wheelRadius = 300; // in planck units
 
-  // maintain theme
-  $effect(() => {
-    if (typeof getThemer().colorHarmony == null) return;
-    if (typeof getThemer().dominant == null) return;
-    if (!balls) return;
-    if (!crank) return;
-    balls!.reTheme();
-    crank!.reTheme();
-  });
+	// maintain theme
+	$effect(() => {
+		if (typeof getThemer().colorHarmony == null) return;
+		if (typeof getThemer().dominant == null) return;
+		if (!balls) return;
+		if (!crank) return;
+		balls!.reTheme();
+		crank!.reTheme();
+	});
 
-  function onResize() {
-    const {innerWidth, innerHeight} = window;
-    balls?.reLayout(innerWidth, innerHeight);
-    crank?.reLayout(innerWidth, innerHeight);
-  }
+	function onResize() {
+		const { innerWidth, innerHeight } = window;
+		balls?.reLayout(innerWidth, innerHeight);
+		crank?.reLayout(innerWidth, innerHeight);
+	}
 
-  // Menu state
-  let palleteMenuOpen = $state(true);
-  let infoModalOpen = $state(false);
-  let winModalOpen = $state(false);
-  let winHistModalOpen = $state(false);
-  let importModalOpen = $state(false);
-  let importModal: ImportModal = $state();
-  let winHistModal: WinHistModal = $state();
+	// Menu state
+	let palleteMenuOpen = $state(true);
+	let infoModalOpen = $state(false);
+	let winModalOpen = $state(false);
+	let winHistModalOpen = $state(false);
+	let importModalOpen = $state(false);
+	let importModal: ImportModal = $state();
+	let winHistModal: WinHistModal = $state();
 
-  // initialize the simulation
-  $effect(() => {
-    if (!Matter()) return;
-    if (!canvasBot) return;
-    if (!canvasTop) return;
-    if (crank || balls) return;
+	// initialize the simulation
+	$effect(() => {
+		if (!Matter()) return;
+		if (!canvasBot) return;
+		if (!canvasTop) return;
+		if (crank || balls) return;
 
-    crank = new CrankSim(canvasTop, wheelRadius + 5);
-    balls = new BallsSim(canvasBot, crank, wheelRadius);
+		crank = new CrankSim(canvasTop, wheelRadius + 5);
+		balls = new BallsSim(canvasBot, crank, wheelRadius);
 
-    crank.onBust.do(() => {
-      balls!.revealBall();
-      canCrank = false;
-    });
+		crank.onBust.do(() => {
+			balls!.revealBall();
+			canCrank = false;
+		});
 
-    // react to winner
-    $effect(() => {
-      if (!balls) return;
-      balls.onReveal.do((i: number) => {
-        canCrank = true;
-        const winnerName = importModal.nameOf(i);
-        if (!winnerName) return;
-        lastWinner = winHistModal.addWinner(winnerName);
-        winModalOpen = true;
-      });
-    });
-  });
+		// react to winner
+		$effect(() => {
+			if (!balls) return;
+			balls.onReveal.do((i: number) => {
+				canCrank = true;
+				const winnerName = importModal.nameOf(i);
+				if (!winnerName) return;
+				lastWinner = winHistModal.addWinner(winnerName);
+				winModalOpen = true;
+			});
+		});
+	});
 
-  // maintain labels
-  type Label = { x: number; y: number; text: string, color: string };
-  let labels: Label[] = $state([]);
-  onMount(() => {
-    setInterval(() => {
-      const pos = balls.ballsPos;
-      const text = pos.map((_, i) => importModal.shortOf(i));
-      labels = text.map((t, i) => ({
-        x: pos[i].x,
-        y: pos[i].y,
-        text: t,
-				color: balls.ballTextColor(i)
-      }));
-    }, 10);
-  });
+	// maintain labels
+	type Label = { x: number; y: number; text: string; color: string };
+	let labels: Label[] = $state([]);
+	onMount(() => {
+		setInterval(() => {
+			const pos = balls.ballsPos;
+			const text = pos.map((_, i) => importModal.shortOf(i));
+			labels = text.map((t, i) => ({
+				x: pos[i].x,
+				y: pos[i].y,
+				text: t,
+				color: balls.ballTextColor(i),
+			}));
+		}, 10);
+	});
 
-  function hitCrank() {
-    if (!canCrank) return;
-    crank?.smackHandle();
-  }
+	function hitCrank() {
+		if (!canCrank) return;
+		crank?.smackHandle();
+	}
 
-  onMount(() => {
-    window["kill"] = () => {
-      crank?.destroy();
-      balls?.destroy();
-    };
-  });
+	onMount(() => {
+		window["kill"] = () => {
+			crank?.destroy();
+			balls?.destroy();
+		};
+	});
 
-  function togglePresent() {
-    if (!lastWinner) return;
-    winHistModal.togglePresent(lastWinner!.id);
-    lastWinner!.markedPresent = !lastWinner!.markedPresent;
-  }
+	function togglePresent() {
+		if (!lastWinner) return;
+		winHistModal.togglePresent(lastWinner!.id);
+		lastWinner!.markedPresent = !lastWinner!.markedPresent;
+	}
 
-  function countWins(): [number, number, number] | undefined {
-    if (!lastWinner) return;
-    return winHistModal.countWins(lastWinner!.name);
-  }
+	function countWins(): [number, number, number] | undefined {
+		if (!lastWinner) return;
+		return winHistModal.countWins(lastWinner!.name);
+	}
 
-  function countWinsToday(): number | undefined {
-    if (!lastWinner) return;
-    return winHistModal.countWinsToday();
-  }
+	function countWinsToday(): number | undefined {
+		if (!lastWinner) return;
+		return winHistModal.countWinsToday();
+	}
 </script>
 
-<svelte:window on:resize={onResize}/>
+<svelte:window on:resize={onResize} />
 
 <div class="fixed z-20 w-screen p-4">
-  <ThemePicker bind:menuOpen={palleteMenuOpen}/>
+	<ThemePicker bind:menuOpen={palleteMenuOpen} />
 </div>
 
 <div class="fixed z-30 top-0">
-  <WinModal
-      winner={lastWinner}
-      {togglePresent}
-      {countWins}
-      {countWinsToday}
-      bind:showModal={winModalOpen}
-  />
+	<WinModal
+		winner={lastWinner}
+		{togglePresent}
+		{countWins}
+		{countWinsToday}
+		bind:showModal={winModalOpen}
+	/>
 </div>
 
 <div class="fixed z-30 top-0">
-  <WinHistModal bind:this={winHistModal} bind:showModal={winHistModalOpen}/>
+	<WinHistModal bind:this={winHistModal} bind:showModal={winHistModalOpen} />
 </div>
 
 <div class="fixed z-30 top-0">
-  <InfoModal bind:showModal={infoModalOpen}/>
+	<InfoModal bind:showModal={infoModalOpen} />
 </div>
 
 <div class="fixed z-30 top-0">
-  <ImportModal bind:this={importModal} bind:showModal={importModalOpen}/>
+	<ImportModal bind:this={importModal} bind:showModal={importModalOpen} />
 </div>
 
-{#each labels as {x, y, text, color}, i}
-  <div
-      class="
+{#each labels as { x, y, text, color }}
+	<div
+		class="
 			absolute
 			-translate-x-1/2
 			-translate-y-1/2
@@ -159,26 +159,27 @@
 			md:text-xl
 			lg:text-2xl
 			xl:text-4xl"
-      style="left: {x}px; top: {y}px; color: {color}">
-    {text}
-  </div>
+		style="left: {x}px; top: {y}px; color: {color}"
+	>
+		{text}
+	</div>
 {/each}
 
 <div class="fixed z-20 bottom-0 right-0">
-  <ControlPanel
-      bind:palleteMenuOpen
-      bind:infoModalOpen
-      bind:importModalOpen
-      bind:winHistModalOpen
-  />
+	<ControlPanel
+		bind:palleteMenuOpen
+		bind:infoModalOpen
+		bind:importModalOpen
+		bind:winHistModalOpen
+	/>
 </div>
 
 <!-- TODO: raise issue: tailwind4 does not recognize svelte5 conditional classes -->
 
 <canvas
-    class={`absolute w-screen h-dvh cursor-grab transition-filter
+	class={`absolute w-screen h-dvh cursor-pointer transition-filter
 		${canCrank ? "active:invert-15" : "invert-35"}`}
-    bind:this={canvasTop}
-    onclick={hitCrank}
+	bind:this={canvasTop}
+	onclick={hitCrank}
 ></canvas>
 <canvas class="absolute w-screen h-dvh -z-10" bind:this={canvasBot}></canvas>
